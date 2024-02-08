e4784056eef2497217709d81346559e265d68928
schaaya <schaaya@umich.edu>
2024-02-08 18:59:43 -0500

    Initial commit

---
+++ src/main/java/com/schaaya/instagramclone/InstagramcloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.schaaya.instagramclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramcloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramcloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schaaya/instagramclone/InstagramcloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schaaya.instagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramcloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
